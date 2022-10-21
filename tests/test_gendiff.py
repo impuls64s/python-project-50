df918dda1e34c5e34a4316dcfca82aeec88c06a1
Oleg Cherkashin <impuls_64@mail.ru>
2022-10-22 00:44:47 +0300

    044

--- tests/test_gendiff.py
+++
@@ -1,7 +0,0 @@
-from gendiff.scripts.gendiff import main
-
-
-def test_main():
-    result = '''usage: gendiff.py [-h] [-f FORMAT] first_file second_file
-gendiff.py: error: the following arguments are required: first_file, second_file'''
-    assert main() == print(result)
